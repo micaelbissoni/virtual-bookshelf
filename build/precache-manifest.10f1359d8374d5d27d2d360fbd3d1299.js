62b5d8793e0797aed684270f1187bed7f34cf48e
Micael Bissoni <micael.bissoni@gateware.com.br>
2020-04-27 13:57:45 -0300

    Deploy

---
+++ build/precache-manifest.10f1359d8374d5d27d2d360fbd3d1299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd0081c376e90fb97813f84bea0c639",
+    "url": "/virtual-bookshelf/index.html"
+  },
+  {
+    "revision": "8aaf5a89e479db86da4d",
+    "url": "/virtual-bookshelf/static/css/main.647810a0.chunk.css"
+  },
+  {
+    "revision": "425a6206be7eae5e1764",
+    "url": "/virtual-bookshelf/static/js/2.a1367df2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/virtual-bookshelf/static/js/2.a1367df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aaf5a89e479db86da4d",
+    "url": "/virtual-bookshelf/static/js/main.79c89377.chunk.js"
+  },
+  {
+    "revision": "54be2869d7ad110b9a20",
+    "url": "/virtual-bookshelf/static/js/runtime-main.36bd8864.js"
+  }
+]);
